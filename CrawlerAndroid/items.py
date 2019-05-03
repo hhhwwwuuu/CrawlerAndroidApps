4f6896ffe9f3db5eecf4ae7ded8c7f1013f9c507
hhhwwwuuu <wzq0515@gmail.com>
2019-05-03 16:36:46 +0900

    get pageNumber

---
+++ CrawlerAndroid/items.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CrawlerandroidItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    category = scrapy.Field()
+    name = scrapy.Field()
+    size = scrapy.Field()
+    packName = scrapy.Field()
+    appId = scrapy.Field()
+    permissionList = scrapy.Field()
+    info = scrapy.Field()
+    downloadLink = scrapy.Field()
+
+class categoryItem(scrapy.Item):
+ 	"""docstring for categoryItem"""
+ 	categoryName = scrapy.Field()
+ 	subLink = scrapy.Field()
+ 	categoryId = scrapy.Field()
+ 	maxPage = scrapy.Field()
+ 		
